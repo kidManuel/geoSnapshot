1779f1a345dfb744394a332cc934d3142040e8e9
kidManuel <cascallarmanuel@gmail.com>
2020-07-15 12:58:19 -0300

    docs folder for deploy

---
+++ docs/precache-manifest.3720731248c003b7bcfd2736753c3edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e827350e5c05deef8993cbd885cb3938",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "94b2f66917bda5b6984e",
+    "url": "/SnapShot/static/css/main.e2592750.chunk.css"
+  },
+  {
+    "revision": "a4e98bc81b4ef028c1fe",
+    "url": "/SnapShot/static/js/2.51178132.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/SnapShot/static/js/2.51178132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b2f66917bda5b6984e",
+    "url": "/SnapShot/static/js/main.9c310c1c.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
